4334d04faea3278bb6cd86edda5fdb5f268e1650
MBlaureNs <42chrono@gmail.com>
2018-04-16 23:09:38 +0000

    incomplete vehicle update

---
+++ priv/repo/update_vehicles.exs
@@ -0,0 +1,45 @@
+defmodule UpdateVehicles do
+  alias TransitTracker.Repo
+  alias TransitTracker.Transit.Vehicle
+  alias TransitTracker.Transit
+  
+  def parse_vehicles([]) do
+  end
+
+  def parse_vehicles(vehicles) do
+    vehicle = hd(vehicles)
+    attr = vehicle["attributes"]
+    route_id = vehicle["relationships"]["route"]["data"]["id"]
+
+    vehicle_params = %Vehicle{
+      mbta_id: vehicle["id"],
+      lat: attr["latitude"],
+      long: attr["longitude"],
+      current_status: attr["current_status"],
+      next_stop_id: nil, #TODO: attr[""],
+      route_id: Transit.get_route_by_route_id!(route_id).id,
+      next_stop_eta: nil, #TODO: attr[""],
+      direction: attr["direction_id"]}
+    
+    Repo.insert!(vehicle_params)
+
+    parse_vehicles(tl(vehicles))
+  end
+
+  def poisondecode(url) do
+    resp = HTTPoison.get!(url)
+    text = Poison.decode!(resp.body)
+    text
+  end
+  
+  def run do
+    Repo.delete_all Vehicle
+    a = poisondecode("https://api-v3.mbta.com/predictions?filter%5Broute%5D=Red,Blue,Green-C,Green-D,Green-E,Green-B&include=vehicle")
+    vehicles = a["included"]
+    predicts = a["data"]
+    
+    parse_vehicles(vehicles)
+  end
+end
+
+UpdateVehicles.run
