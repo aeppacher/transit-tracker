5b0b32bca6dd4f5db127b9f46d9f7feee337124c
aeppacher <alessandroeppacher@gmail.com>
2018-04-16 16:25:53 -0400

    style stops

--- assets/js/cs/stop.jsx
+++ assets/js/cs/stop.jsx
@@ -5,14 +5,36 @@ export default function Stop(params) {
   let stop = params.stop;
 
   let stopName = stop.name;
-  let stopRoutes = stop.routes;
+  let stopRoutes = _.map(stop.routes, (rr) => 
+    <CardText style={{ backgroundColor: getColor(rr), color: "#ffffff"}}>
+    {rr}
+    </CardText>);
 
-  return <Card>
+  let cardStyle = {
+    backgroundColor: '#343a40',
+    borderColor: '#333',
+    width: '200px',
+    padding: '0px',
+    margin: '5px'
+  };
+
+  return <Card body inverse style={cardStyle} className="text-center">
     <CardBody>
-      <div>
         <CardTitle>{stopName}</CardTitle>
-        <CardText>{stopRoutes}</CardText>
-      </div>
+        {stopRoutes}
     </CardBody>
   </Card>;
+}
+
+function getColor(name){
+  switch(name){
+    case "Red":
+      return "#f44242";
+    case "Blue":
+      return "#4286f4";
+    case "Orange":
+      return "#c97800";
+    default:
+      return "#2d964e";
+  }
 }
